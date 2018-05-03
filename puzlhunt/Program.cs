8b96d13331b3ad56df59b8cc9ebccf010461a89d
mrbrun <robert.hook@unf.se>
2018-05-03 12:52:18 +0200

    Add project files.

---
+++ puzlhunt/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace puzlhunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                string welcomeText = "Welcome to UNF Puzlhunt \nPlease enter your password.\nPassword:\n";
+                TypeLine(welcomeText);
+
+                string answer = Console.ReadLine();
+
+                if(answer == "QWERTY") // Läs vad Password är
+                {
+                    System.Threading.Thread.Sleep(3000); // Sleep för att det ska se ut som att det laddar.
+                    Console.Clear();
+                    string password = "Password: \n";
+                    TypeLine(password);
+
+                    string pwGuess = Console.ReadLine();
+                }
+                else
+                {
+                    Console.Clear();
+                    string wrongPassword = "Please try again...";
+                    TypeLine(wrongPassword);
+                }
+
+            }
+        }
+
+        static void writeASCII()
+        {
+                Console.WriteAscii("UNF PUZLHUNT", Color.Lime);
+        }
+
+        static void TypeLine(string line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                Console.Write(line[i]);
+                System.Threading.Thread.Sleep(50); // Sleep for 150 milliseconds
+            }
+        }
+    }
+}
